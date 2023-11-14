9191de10cfdf0b932b77cadcbc31aa419f809de3
uquant0507 <99530149+uquant0507@users.noreply.github.com>
2023-11-14 17:18:23 +0900

    capture_signal

--- sniper/capture_signal.py
+++ sniper/capture_signal.py
@@ -1,16 +1,20 @@
 import time
 import os
 import sys
+import asyncio
 current_script_path = os.path.abspath(__file__)
 project_root_path = os.path.abspath(os.path.join(current_script_path, '..', '..', 'src'))
 sys.path.append(project_root_path)
-from utils import crawling
+import crawling
 
 BINANCE_URL = "https://www.binance.com/en/support/announcement/new-cryptocurrency-listing?c=48&navId=48"
 
-while(True):
-    signal = crawling.announce_check(BINANCE_URL, 1)
-    print(signal)
-    if signal:
-        pass
-    time.sleep(30)
+
+
+async def capture_signal():
+    while(True):
+        signal = crawling.announce_check(BINANCE_URL, 1)
+        print(signal)
+        if signal:
+            pass
+        await asyncio.sleep(30)
